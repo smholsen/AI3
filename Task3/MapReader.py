c1c0659a1d75a056838185d72673b25838de65f8
Simon <smholsen@stud.ntnu.no>
2016-09-26 14:38:18 +0200

    Structure changes. Starte implementing map interpreter.

---
+++ Task3/MapReader.py
@@ -0,0 +1,30 @@
+class MapReader:
+
+    # Map options
+    options = {
+        '11': "board-1-1.txt",
+        '12': "board-1-2.txt",
+        '13': "board-1-3.txt",
+        '14': "board-1-4.txt",
+        '21': "board-2-1.txt",
+        '22': "board-2-2.txt",
+        '23': "board-2-3.txt",
+        '24': "board-2-4.txt"
+    }
+
+    @staticmethod
+    def read():
+        map = []
+        # Select map
+        board = input("Enter board level (11, 12, 13, 14, 21, 22, 23 or 24)")
+        boardFile = MapReader.options[board]
+        # Read map file
+        f = open('../Maps/' + boardFile, 'r')
+        for line in f:
+            print(line)
+
+
+        return map
+
+
+MapReader.read()
